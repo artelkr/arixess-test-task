358bc3aee37696b453f1d9006b18b0b8fb88bd4e
Artem Kryukov <artelkryukov@gmail.com>
2021-10-08 20:02:53 +0300

    Add registration

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $attributes = [
+        'role' => self::ROLE_USER,
+    ];
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,32 @@
 <x-auth-layout>
-    <h1>Register Form</h1>
+    <x-slot name="page_title">
+        Register Form
+    </x-slot>
+
+    <form action="{{ route('register') }}" method="post">
+        @csrf
+
+        <label>
+            Name
+
+            <input type="text" autocomplete="name" name="name" required value="{{ old('name') }}">
+        </label>
+        <label>
+            Email
+
+            <input type="email" name="email" required value="{{ old('email') }}">
+        </label>
+        <label>
+            Password
+
+            <input type="password" name="password" autocomplete="new-password" required minlength="8">
+        </label>
+        <label>
+            Password Confirmation
+
+            <input type="password" autocomplete="new-password" name="password_confirmation" required minlength="8">
+        </label>
+
+        <button type="submit">Send</button>
+    </form>
 </x-auth-layout>
